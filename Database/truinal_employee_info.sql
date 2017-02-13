9c3e6ff05afbfc5d58b842b07a6f4395e670374f
rizwan090045 <rizwanul.khan@valpo.edu>
2017-02-12 19:35:40 -0600

    updated_account_info
    updated account info with query

--- Database/truinal_employee_info.sql
+++ Database/truinal_employee_info.sql
@@ -29,7 +29,19 @@ select * from account;
 
 desc employee_info;
 
-insert into employee_info values ( 12345, "peter", "null", "sedrak", "valparaiso", "grad school", "valparaiso", "IN", 46383, "USA", '12-12-12' ) ;
+insert into employee_info values ( 12345, "peter", "null", "sedrak", "valparaiso", "grad school", "valparaiso", "IN", 46383, "USA", '12-12-12', null, null ) ;
+insert into account values 
+((12345,"peter",12345), (3888256,"rkhan1",3888256), (3912003, "cfcalina", 3912003), 
+(3913019, "ngopalapuram", 3913019), (3913259, "rsmallela", 3913259), (3914624, "nrbandakunta",3914624),
+(3915358, "smeda", 3915358), (3915467,"srpolangari", 3915467), (3915607,"ndburri",3915607), 
+(3921989,"rmathew",3921989), (3922707,"rbalusu",3922707), (3925550,"nyeluripati",3925550), 
+(3940165,"apola1",3940165));
+
+
+
+
+
+
 
 
 select * from employee_info;
